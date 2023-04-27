10b32ea9805ac82e6f0031c36f5add581f80ca8c
Ava's DAO <support@avasdao.org>
2023-04-26 20:00:07 -0400

    Add Wally seed backup help.

--- pages/Help.vue
+++ pages/Help.vue
@@ -6,14 +6,42 @@
             </h2>
         </div>
 
-        <p class="mt-5 px-3 sm:px-10 py-3">
-            <a href="mailto:support@avasdao.org" class="text-2xl text-blue-500 font-medium hover:underline">
+        <section class="mt-5 px-3 sm:px-10 py-3">
+            <h2 class="text-xl font-medium">
+                Don't hesitate to reach our for support
+            </h2>
+
+            <a href="mailto:support@avasdao.org" class="text-2xl text-blue-500 font-bold hover:underline">
                 support@avasdao.org
             </a>
-        </p>
+        </section>
 
-        <p class="px-3 sm:px-10 py-3">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
+        <section class="px-3 sm:px-10 py-3">
+            <h1 class="text-2xl font-medium">
+                Backup Your Wally Dice Seed Phrase
+            </h1>
+
+            <p class="mt-3">
+                It's critically important to backup your seed phrase <span class="font-bold">-OR-</span> you risk losing <span class="font-bold">ALL OF YOUR ASSETS</span>
+            </p>
+
+            <ol class="mt-3 ml-10 list-decimal">
+                <li>
+                    Under <span class="font-bold">Accounts</span>, click on one of your accounts.
+                </li>
+
+                <li>
+                    Click on the <span class="font-bold">gear icon</span> that appears on the right-hand side of your respective account.
+                </li>
+
+                <li>
+                    Click on <span class="font-bold">"View Account Recovery Secret Phrase".</span>
+                </li>
+
+                <li>
+                    Write down the <span class="font-bold">12 words</span> shown in red and store them in a <span class="font-bold">VERY SAFE PLACE.</span>
+                </li>
+            </ol>
+        </section>
     </main>
 </template>
