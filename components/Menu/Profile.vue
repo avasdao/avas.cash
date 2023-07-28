bb788a0d211a26e58983e5b7570a98234af30902
Ava's DAO <support@avasdao.org>
2023-07-28 13:41:46 -0400

    Update profile.

--- components/Menu/Profile.vue
+++ components/Menu/Profile.vue
@@ -16,7 +16,9 @@ const hasAuth = ref(false)
 const isLoading = ref(true)
 
 const nickname = ref(null)
-nickname.value = 'Satoshi'
+const email = ref(null)
+const fullName = ref(null)
+const socials = ref(null)
 
 const scrollToTop = () => {
     window.scrollTo({
@@ -26,115 +28,60 @@ const scrollToTop = () => {
     })
 }
 
-const pollForAuth = async () => {
-    console.log('POLLING FOR AUTH')
-
-    if (!Profile.sessionid) {
-        /* Handle loading flag. */
-        if (isLoading.value) {
-            isLoading.value = false
-        }
-
-        return console.error('Oops! We DO NOT have an active Session.')
-    }
-
-    /* Set target. */
-    const target = '/v1/session/' + Profile.sessionid
-    console.log('TARGET', target)
-
-    const session = await $fetch(target)
-    console.log('SESSION', session)
-
-    /* Validate authorized session. */
-    if (session?.profileid) {
-        /* Set authorization flag. */
-        hasAuth.value = true
-
-        /* Scroll to page top. */
-        scrollToTop()
-
-        /* Stop polling. */
-        if (pollingid) {
-            clearInterval(pollingid)
-        }
-
-        /* Save session to profile. */
-        Profile.saveSession(session)
-    }
-
-    /* Handle loading flag. */
-    if (isLoading.value) {
-        isLoading.value = false
-    }
-}
-
-/**
- * Sign Out
- *
- * Deletes ALL stored values from the browsers (IndexedDB) cache.
- */
- const signOut = () => {
-    /* Delete ALL session data. */
-    Profile.deleteSession()
-
-    /* Initialize route handler. */
-    const router = useRouter()
-
-    /* Go to homepage. */
-    router.replace('/')
-}
 
 /* Handle mounting. */
 onMounted(() => {
     /* Setup API polling. */
-    pollForAuth()
+    // pollForAuth()
 
     /* Initialize authorization polling. */
     // FIXME How can we implement WebSockets for more efficiency?
-    pollingid = setInterval(pollForAuth, POLLING_FREQUENCY)
+    // pollingid = setInterval(pollForAuth, POLLING_FREQUENCY)
 })
 
 onBeforeUnmount(() => {
     /* Stop polling. */
-    if (pollingid) {
-        clearInterval(pollingid)
-    }
+    // if (pollingid) {
+    //     clearInterval(pollingid)
+    // }
 })
 </script>
 
 <template>
     <main class="px-3 max-w-5xl mx-auto text-gray-700">
-        <div class="pt-10 flex justify-center">
-            <h1 v-if="isLoading || hasAuth" class="text-3xl sm:text-5xl font-bold tracking-widest text-center">
-                My Profile
-            </h1>
-            <h1 v-else class="text-3xl sm:text-5xl font-bold tracking-widest text-center">
-                Authorization Required
-            </h1>
-        </div>
+        <h1 class="text-2xl font-medium">
+            Profile
+        </h1>
 
-        <section v-if="isLoading" class="pt-10 py-10 flex justify-center">
-            <h1 class="text-2xl">
-                loading, please wait...
-            </h1>
+        <section class="my-5 flex flex-col gap-8">
+            <input
+                class="w-full px-3 py-1 text-lg sm:text-xl bg-yellow-200 border-2 border-yellow-400 rounded-md shadow"
+                type="text"
+                v-model="nickname"
+                placeholder="(optional) Enter your nickname"
+            />
+
+            <input
+                class="w-full px-3 py-1 text-lg sm:text-xl bg-yellow-200 border-2 border-yellow-400 rounded-md shadow"
+                type="text"
+                v-model="email"
+                placeholder="(optional) Enter your email address"
+            />
+
+            <input
+                class="w-full px-3 py-1 text-lg sm:text-xl bg-yellow-200 border-2 border-yellow-400 rounded-md shadow"
+                type="text"
+                v-model="fullName"
+                placeholder="(optional) Enter your full name"
+            />
+
+            <input
+                class="w-full px-3 py-1 text-lg sm:text-xl bg-yellow-200 border-2 border-yellow-400 rounded-md shadow"
+                type="text"
+                v-model="socials"
+                placeholder="(optional) Enter your social media"
+            />
         </section>
 
-        <div v-else>
-            <section v-if="hasAuth" class="py-10 flex flex-col items-center gap-10">
-                <h2 class="text-3xl text-rose-500 font-bold">
-                   Nexa Exchange Profile
-                </h2>
-
-                <p class="max-w-lg text-xl text-center">
-                    Please stand by as we prepare your Exchange profile page.
-                </p>
-
-                <button @click="signOut" class="px-5 py-2 bg-red-500 text-xl text-red-50 font-medium border-4 border-red-700 rounded-md shadow-md hover:bg-red-600">
-                    Sign Out
-                </button>
-            </section>
-
-            <AuthView v-else />
-        </div>
     </main>
 </template>
