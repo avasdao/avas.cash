9898c179d6d3402f8bf7a14fbb01994ff0458ca2
Ava's DAO <support@avasdao.org>
2023-07-31 22:50:48 -0400

    Rename.

--- pages/staking.vue
+++ pages/staking.vue
@@ -158,9 +158,5 @@ const System = useSystemStore()
                 </h4>
             </div>
         </section>
-
-        <!-- <p class="px-3 sm:px-10 py-3">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p> -->
     </main>
 </template>
