9e5742d8cc934cfec6031571a41100ed9f68df66
Ava's DAO <support@avasdao.org>
2023-08-20 21:38:19 -0400

    Add Firebase config.

--- stores/system.ts
+++ stores/system.ts
@@ -14,6 +14,7 @@ import { instantiateRipemd160 } from '@bitauth/libauth'
 /* Libauth helpers. */
 import { encodeDataPush } from '@bitauth/libauth'
 
+/* Import clipboard manager. */
 import './system/clipboard.ts'
 
 /* Initialize constants. */
