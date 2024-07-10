e0d2983f716c05672b4ad83726a65c020cee65d7
Ava's DAO <support@avasdao.org>
2024-07-10 11:36:20 -0400

    Fix settings.

--- components/Menu/Settings.vue
+++ components/Menu/Settings.vue
@@ -16,11 +16,11 @@ const currency = ref(null)
 
 
 const wordList = computed(() => {
-    if (!Wallet.mnemonic) {
+    if (!Wallet.wallet.mnemonic) {
         return null
     }
 
-    const list = Wallet.mnemonic.split(' ')
+    const list = Wallet.wallet.mnemonic.split(' ')
 
     return list
 })
@@ -125,158 +125,7 @@ currency.value = 'USD'
 
         <div class="mx-5 my-5 border-t border-gray-300" />
 
-        <fieldset v-if="1 === 2">
-            <legend class="block text-sm font-medium leading-6 text-gray-900">
-                Choose a Theme Color
-            </legend>
-
-            <div class="mt-4 flex items-center space-x-3">
-    <!--
-      Active and Checked: "ring ring-offset-1"
-      Not Active and Checked: "ring-2"
-    -->
-                <label class="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-pink-500">
-                    <input type="radio" name="color-choice" value="Pink" class="sr-only" aria-labelledby="color-choice-0-label">
-                    <span id="color-choice-0-label" class="sr-only">Pink</span>
-                    <span aria-hidden="true" class="h-8 w-8 bg-pink-500 rounded-full border border-black border-opacity-10"></span>
-                </label>
-    <!--
-      Active and Checked: "ring ring-offset-1"
-      Not Active and Checked: "ring-2"
-    -->
-                <label class="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-purple-500">
-                    <input type="radio" name="color-choice" value="Purple" class="sr-only" aria-labelledby="color-choice-1-label">
-                    <span id="color-choice-1-label" class="sr-only">Purple</span>
-                    <span aria-hidden="true" class="h-8 w-8 bg-purple-500 rounded-full border border-black border-opacity-10"></span>
-                </label>
-    <!--
-      Active and Checked: "ring ring-offset-1"
-      Not Active and Checked: "ring-2"
-    -->
-                <label class="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-blue-500">
-                    <input type="radio" name="color-choice" value="Blue" class="sr-only" aria-labelledby="color-choice-2-label">
-                    <span id="color-choice-2-label" class="sr-only">Blue</span>
-                    <span aria-hidden="true" class="h-8 w-8 bg-blue-500 rounded-full border border-black border-opacity-10"></span>
-                </label>
-    <!--
-      Active and Checked: "ring ring-offset-1"
-      Not Active and Checked: "ring-2"
-    -->
-                <label class="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-green-500">
-                    <input type="radio" name="color-choice" value="Green" class="sr-only" aria-labelledby="color-choice-3-label">
-                    <span id="color-choice-3-label" class="sr-only">Green</span>
-                    <span aria-hidden="true" class="h-8 w-8 bg-green-500 rounded-full border border-black border-opacity-10"></span>
-                </label>
-    <!--
-      Active and Checked: "ring ring-offset-1"
-      Not Active and Checked: "ring-2"
-    -->
-                <label class="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-yellow-500">
-                    <input type="radio" name="color-choice" value="Yellow" class="sr-only" aria-labelledby="color-choice-4-label">
-                    <span id="color-choice-4-label" class="sr-only">Yellow</span>
-                    <span aria-hidden="true" class="h-8 w-8 bg-yellow-500 rounded-full border border-black border-opacity-10"></span>
-                </label>
-            </div>
-        </fieldset>
-
-
-        <div v-if="1 === 2">
-            <label for="combobox" class="block text-sm font-medium leading-6 text-gray-900">
-                Save My Games
-            </label>
-
-            <div class="relative mt-2">
-                <input
-                    id="combobox"
-                    type="text"
-                    class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    role="combobox"
-                    aria-controls="options"
-                    aria-expanded="false"
-                    value="Forever">
-
-                <button type="button" class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-                    <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd" />
-                    </svg>
-                </button>
-
-                <ul v-if="isShowingSaveGamesOptions" class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" id="options" role="listbox">
-    <!--
-    Combobox option, manage highlight styles based on mouseenter/mouseleave and keyboard navigation.
-
-    Active: "text-white bg-indigo-600", Not Active: "text-gray-900"
-    -->
-                    <li class="relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900" id="option-0" role="option" tabindex="-1">
-    <!-- Selected: "font-semibold" -->
-                        <span class="block truncate">Daily</span>
-
-    <!--
-        Checkmark, only display for selected option.
-
-        Active: "text-white", Not Active: "text-indigo-600"
-    -->
-                        <span class="absolute inset-y-0 right-0 flex items-center pr-4 text-white">
-                            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
-                    </li>
-
-                    <li class="relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900" id="option-0" role="option" tabindex="-1">
-    <!-- Selected: "font-semibold" -->
-                        <span class="block truncate">1 Week</span>
-
-    <!--
-        Checkmark, only display for selected option.
-
-        Active: "text-white", Not Active: "text-indigo-600"
-    -->
-                        <span class="absolute inset-y-0 right-0 flex items-center pr-4 text-white">
-                            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
-                    </li>
-
-                    <li class="relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900" id="option-0" role="option" tabindex="-1">
-    <!-- Selected: "font-semibold" -->
-                        <span class="block truncate">1 Month</span>
-
-    <!--
-        Checkmark, only display for selected option.
-
-        Active: "text-white", Not Active: "text-indigo-600"
-    -->
-                        <span class="absolute inset-y-0 right-0 flex items-center pr-4 text-white">
-                            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
-                    </li>
-
-                    <li class="relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900" id="option-0" role="option" tabindex="-1">
-    <!-- Selected: "font-semibold" -->
-                        <span class="block truncate font-semibold">Forever</span>
-
-    <!--
-        Checkmark, only display for selected option.
-
-        Active: "text-white", Not Active: "text-indigo-600"
-    -->
-                        <span class="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600">
-                            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
-                    </li>
-
-    <!-- More items... -->
-                </ul>
-            </div>
-        </div>
-
-        <section v-if="Wallet.mnemonic" class="mb-5 px-3 py-2 bg-red-300 border border-red-500 rounded-lg shadow">
+        <section v-if="Wallet.address" class="mb-5 px-3 py-2 bg-red-300 border border-red-500 rounded-lg shadow">
             <h2 class="text-2xl text-red-900 font-bold">
                 Danger Zone
             </h2>
